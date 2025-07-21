3dad51a3339cf765c7d97ed566328efb7fa472d8
Will <whemsworth@hotmail.co.uk>
2025-07-21 21:28:54 +0100

    basic working example with find_package working

--- examples/test_sim/test_sim.cpp
+++ examples/test_sim/test_sim.cpp
@@ -1,7 +1,9 @@
-#include <types.h>
+#include <bitloop/core/types.h>
+#include <iostream>
 
 int main()
 {
 	int age = getAge();
+    std::cout << "Age: " << age << std::endl;
 	return 0;
 }
--- framework/src/core/types.cpp
+++ framework/src/core/types.cpp
@@ -3,5 +3,5 @@
 
 int getAge()
 {
-   return 31;
+   return 560;
 }
